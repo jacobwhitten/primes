9d734e128c330fd1dfcc2414496745b8cb60f46b
Thane Gill <me@thanegill.com>
2014-05-10 16:56:01 -0700

    Much bigger ints!

--- primearray.c
+++ primearray.c
@@ -11,26 +11,26 @@ Date: 5/9/14
 #include <string.h>
 #include <math.h>
 
-void printprimes(unsigned char array[], int size){
-    int i;
-    for(i=0; i < size; i++){
+void printprimes(unsigned char array[], unsigned long long int size){
+    unsigned long long int i;
+    for(i=size; i > 0; i--){
         if(array[i] == 1){
-            fprintf(stdout, "%d%s", i, "\n" );
+            fprintf(stdout, "%llu%s", i, "\n" );
         }
     }
 }
 
-int assertprimes(unsigned char array[], int size){
-    int p; //array index
+int assertprimes(unsigned char array[], unsigned long long int size){
+    unsigned long long int p; //array index
     int rtn = 0; //return value
 
     // Loop though array
     for(p = 3; p < size; p++) {
         if (array[p] == 1) { // p is prime
-            int i = ((int) sqrt(p)) + 1;
+            unsigned long long int i = ((unsigned long long int) sqrt(p)) + 1; // Start at the sqrt + 1 of p
             while(i > 1){ // loop backwards though primes checking for divisibility
                 if ((array[i] == 1) && ((p % i) == 0)){
-                    fprintf(stderr, "%d is not a prime!\n", p);
+                    fprintf(stderr, "%llu is not a prime!\n", p);
                     i = 1; // already not prime, not worth checking anything else
                     rtn = 1;
                 }
@@ -47,23 +47,23 @@ int main(int argc, char **argv){
         fprintf(stderr, "%s\n", "Usage is: primearray limit");
         return 1;
     }
-    int arraysize = atoi(argv[1]); // size of array
+    unsigned long long int arraysize = atoi(argv[1]); // size of array
     unsigned char primearray[arraysize]; // Allocate memory to array on the stack
 
 
-    memset(primearray, 1, sizeof(unsigned char)*arraysize); //set all elements to 1, prime
+    memset(primearray, 1, sizeof(unsigned char) * (arraysize)); //set all elements to 1, prime
 
     //set 0 and 1 to 0, not prime.
     primearray[0] = 0;
     primearray[1] = 0;
 
-    double max = sqrt(arraysize); //max check
-    int i = 2; //index of primearray
+    long double max = sqrt(arraysize); //max check
+    unsigned long long int i = 2; //index of primearray
 
     while(i < max) {
         if(primearray[i] == 1) {
-            int p = i;
-            int check = i+i;
+            unsigned long long int p = i;
+            unsigned long long int check = i+i;
             /* printf("%d\n",i); */
             while(check < arraysize) {
                 /* printf("%d\n", i); */
@@ -78,7 +78,7 @@ int main(int argc, char **argv){
 
     printprimes(primearray, arraysize);
 
-    assertprimes(primearray, arraysize);
+    /* assertprimes(primearray, arraysize); */
 
     return 0;
 }
